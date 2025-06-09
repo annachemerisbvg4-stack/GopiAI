d25c7ff7e66cee74be502e5bc89fe293f3b7163d
amritagopi <anna.chemeris.bvg@gmail.com>
2025-06-09 19:12:25 +0530

    Auto-backup: 20250906_191225

--- auto_backup.bat
+++ auto_backup.bat
@@ -1,4 +1,6 @@
 @echo off
+
+chcp 65001 >nul || exit /b
 REM Скрипт автоматического бэкапа
 set timestamp=%date:~-4,4%%date:~-10,2%%date:~-7,2%_%time:~0,2%%time:~3,2%%time:~6,2%
 set timestamp=%timestamp: =0%
